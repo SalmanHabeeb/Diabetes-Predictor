14ec2d3c420ae4c560cc5a7a55c3e65ecf148743
Syed Salman Habeeb Quadri <98826220+SalmanHabeeb@users.noreply.github.com>
2022-08-14 18:59:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import pickle
-import sklearn
+from sklearn.linear_model import LogisticRegression
 from flask import Flask, render_template, request
 
 app=Flask(__name__, template_folder = "assets/templates")
