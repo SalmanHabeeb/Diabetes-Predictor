e770cd789e57188653d928e7b88dbc04fd6a11b7
Syed Salman Habeeb Quadri <98826220+SalmanHabeeb@users.noreply.github.com>
2022-08-18 19:10:46 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-from email import message
-import pandas as pd
 import numpy as np
 import pickle
 from flask import Flask, render_template, request
@@ -13,29 +11,18 @@ def home():
 @app.route('/predict', methods = ['GET','POST'])
 def predict():
     
-    if request.method == 'POST':
-        print(request.form.get('no_of_pregnancies'))
-        print(request.form.get('glucose_level'))
-        print(request.form.get('insulin'))
-        print(request.form.get('bmi'))
-        print(request.form.get('dpf'))
-        print(request.form.get('age'))
-        
+    if request.method == 'POST':        
         try:
             no_of_pregnancies = float(request.form['no_of_pregnancies'])
             glucose_level = float(request.form['glucose_level'])
-            insulin = float(request.form['insulin'])
-            bmi = float(request.form['bmi'])
-            dpf = float(request.form['dpf'])
+            weight = float(request.form['weight'])
+            height = float(request.form['height'])
             age = float(request.form['age'])
             
-            insulin = np.log1p(insulin)
+            bmi = weight/(height/100)**2
             
-            entries = [no_of_pregnancies, glucose_level, insulin, bmi, dpf, age]
+            entries = [no_of_pregnancies, glucose_level, bmi, age]
             entries = np.array(entries)
-            scaling_df = pd.read_csv("assets/scaling_data.csv")
-            for i, entry in enumerate(entries):
-                entries[i] = (entry - scaling_df.iloc[0, i + 1])/scaling_df.iloc[1, i + 1]
             entries = entries.reshape(1, -1)
             with open("assets/model.dat", "rb") as f:
                 lr_model = pickle.load(f)
